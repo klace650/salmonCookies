2da03190fe6fbeca0b523d98d9ef0d606c6b8cb0
Klace Koch <koch.klace@gmail.com>
2020-08-06 19:34:13 -0700

    Random customer generator working at least once

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 'use strict'
+// Math.random script from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Seattle Store
+
 var seaStore = {
   minCustomer: 23,
   maxCustomer: 65,
   averageSale: 6.3, //its per visit, bud.
 
   // Generate random customer traffic
-  // generateTraffic: function getRandomInt(min,max){
-  //   this.min = Math.ceil(min);
-  //   this.max = Math.floor(max);
-  //   return Math.floor(Math.random()*(max-min)+min);
-
+  generateTraffic: function(){
+    var pickANumber = getRandomIntInclusive(this.minCustomer,this.maxCustomer);
+    return pickANumber;
+    
+  },
     // within the upper and lower limits of customer capacity
-
 
   // Simulate sales using random traffic and averageSale
 
-
-
 };
-
-console.log(seaStore.w3Test)
+console.log(seaStore.generateTraffic());
