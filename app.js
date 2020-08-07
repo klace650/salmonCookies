50b204369e9ee2e0ff31d08545d9fb074e3c7ada
Klace Koch <koch.klace@gmail.com>
2020-08-06 19:55:44 -0700

    Simulated sales function done

--- app.js
+++ app.js
@@ -13,16 +13,24 @@ var seaStore = {
   minCustomer: 23,
   maxCustomer: 65,
   averageSale: 6.3, //its per visit, bud.
+  soldCookies: [],
 
   // Generate random customer traffic
+  // within limits
   generateTraffic: function(){
     var pickANumber = getRandomIntInclusive(this.minCustomer,this.maxCustomer);
-    return pickANumber;
-    
+    return pickANumber; 
   },
-    // within the upper and lower limits of customer capacity
-
   // Simulate sales using random traffic and averageSale
-
+  //just need to multiply generated traffic count and averageSale:
+  // needs to run for each hour - for loop i reckon?
+  simulateSales: function(){
+    var runSim = this.generateTraffic() * this.averageSale
+    return runSim;
+  },
 };
-console.log(seaStore.generateTraffic());
+
+
+
+console.log(seaStore.generateTraffic()); //seaStore.generateTraffic produces one random number between 23 and 60. yus.
+console.log(seaStore.simulateSales()); // so far this just shows one hour
