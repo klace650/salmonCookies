338610997f35a4b38dc729cb6084e831b12b0839
Klace Koch <koch.klace@gmail.com>
2020-08-06 19:17:35 -0700

    First object done, working on methods.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict'
+var seaStore = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  averageSale: 6.3, //its per visit, bud.
+
+  // Generate random customer traffic
+  // generateTraffic: function getRandomInt(min,max){
+  //   this.min = Math.ceil(min);
+  //   this.max = Math.floor(max);
+  //   return Math.floor(Math.random()*(max-min)+min);
+
+    // within the upper and lower limits of customer capacity
+
+
+  // Simulate sales using random traffic and averageSale
+
+
+
+};
+
+console.log(seaStore.w3Test)
