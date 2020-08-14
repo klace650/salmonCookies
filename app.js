a43ff10270dedbc73234fcc5cef83308c635ac41
Klace Koch <koch.klace@gmail.com>
2020-08-13 20:40:57 -0700

    Whew that was tough - got console logs to produce results from constructor - single store

--- app.js
+++ app.js
@@ -15,25 +15,30 @@ function Store(location, minCustomer, maxCustomer, averageSale){
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.averageSale = averageSale;
-  // this.soldCookies = soldCookies;
-}
+  this.hourlySales = [];
+  this.businessHours = [' 6am',' 7am',' 8am',' 9am',' 10am',' 11am',' 12pm',' 1pm',' 2pm',' 3pm',' 4pm',' 5pm',' 6pm',' 7pm'];
+};
+
 Store.prototype.generateTraffic = function(){
-  var pickANumber = getRandomIntInclusive(this.minCustomer,this.maxCustomer);
-    return pickANumber;
-}
+    var min = this.minCustomer;
+    var max = this.maxCustomer;
+    var randomNumber = Math.random()*(max-min)+ min;
+    return Math.floor(randomNumber);
+};
+
 Store.prototype.simulateSales = function(){
   for (var i = 0; i < 14; i++){
-    var runSim = this.generateTraffic() * this.averageSale;
-    var roundedSales = Math.floor(runSim);
-    this.soldCookies[i] = roundedSales;
-}
-}  
+    var soldCookies = this.generateTraffic()*this.averageSale;
+    this.hourlySales.push(soldCookies);
+    return Math.floor(soldCookies);
+  }
+};
+ var seaStore = new Store('Seattle', 23, 65, 6.3)
 
-var seaStore = new Store('Seattle',23,65,6.3);
+console.log(seaStore.generateTraffic());
+console.log(seaStore.simulateSales());
+  // seaStore.simulateSales();
 
-// console.log(seaStore.simulateSales());
-
-seaStore.simulateSales()
 
 //************Start of Objects**************//
 
