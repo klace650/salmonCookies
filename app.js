04c5f72057aafc9bf21ee64683a43c405e25d51d
Klace Koch <koch.klace@gmail.com>
2020-08-17 17:54:39 -0700

    starting again

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ function Store(location, minCustomer, maxCustomer, averageSale){
   this.businessHours = [' 6am',' 7am',' 8am',' 9am',' 10am',' 11am',' 12pm',' 1pm',' 2pm',' 3pm',' 4pm',' 5pm',' 6pm',' 7pm', 'Total'];
 };
 
+
+
 Store.prototype.generateTraffic = function(){
   var min = this.minCustomer;
   var max = this.maxCustomer;
   var randomNumber = Math.random()*(max-min)+ min;
     return Math.floor(randomNumber);
 };
+
 
 
 Store.prototype.simulateSales = function(){
@@ -28,6 +31,8 @@ Store.prototype.simulateSales = function(){
   }
 };
 
+
+
 Store.prototype.getTotals = function(){
   var cookieTotal = 0;
   for (var i = 0; i < this.hourlySales.length; i++) {
@@ -35,11 +40,9 @@ Store.prototype.getTotals = function(){
   };
   this.hourlySales.push(cookieTotal);
 };
-Store.prototype.tryRender = function(){
-var parentSales = document.getElementById('salesData');
-var childSales = document.createElement('li');
-parentSales.appendChild(childSales);
-}
+
+
+
 Store.prototype.salesHeader = function(){
   for (var i = 0; i < this.businessHours.length; i++){
     var headerParent = document.getElementById('salesHeader');
@@ -49,12 +52,24 @@ Store.prototype.salesHeader = function(){
   }
 }
 
+
+
+// Store.prototype.salesCell = function(){
+//   for (var i = 0; i < this.hourlySales.length; i++){
+//     var cellParent = document.getElementById('salesCell');
+//     var cellChild = document.createElement('td');
+//     cellChild.textContent = this.hourlySales[i];
+//     cellParent.appendChild(cellChild);
+//   }
+// }
+
 // STORE STATS //
  var seaStore = new Store('Seattle', 23, 65, 6.3)
 //  var testStore = new Store ('Test', 21, 45, 5.2)
 
- // CONSOLE LOGS //
-seaStore.salesHeader();
+// CONSOLE LOGS //
 seaStore.simulateSales();
 seaStore.getTotals();
+seaStore.salesHeader();
+seaStore.salesCell();
 console.log(seaStore.hourlySales);
