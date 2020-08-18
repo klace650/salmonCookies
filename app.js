1da2a09abe8e7b6183a91e783efff620abaf2867
Klace Koch <koch.klace@gmail.com>
2020-08-17 20:38:12 -0700

    Still playing with table header

--- app.js
+++ app.js
@@ -11,16 +11,12 @@ function Store(location, minCustomer, maxCustomer, averageSale){
   this.businessHours = [' 6am',' 7am',' 8am',' 9am',' 10am',' 11am',' 12pm',' 1pm',' 2pm',' 3pm',' 4pm',' 5pm',' 6pm',' 7pm', 'Total'];
 };
 
-
-
 Store.prototype.generateTraffic = function(){
   var min = this.minCustomer;
   var max = this.maxCustomer;
   var randomNumber = Math.random()*(max-min)+ min;
     return Math.floor(randomNumber);
 };
-
-
 
 Store.prototype.simulateSales = function(){
   for (var i = 0; i < 14; i++){
@@ -31,8 +27,6 @@ Store.prototype.simulateSales = function(){
   }
 };
 
-
-
 Store.prototype.getTotals = function(){
   var cookieTotal = 0;
   for (var i = 0; i < this.hourlySales.length; i++) {
@@ -40,36 +34,34 @@ Store.prototype.getTotals = function(){
   };
   this.hourlySales.push(cookieTotal);
 };
-
-
-
-Store.prototype.salesHeader = function(){
-  for (var i = 0; i < this.businessHours.length; i++){
-    var headerParent = document.getElementById('salesHeader');
-    var headerChild = document.createElement('th');
-    headerChild.textContent = this.businessHours[i];
-    headerParent.appendChild(headerChild);
+  
+Store.prototype.renderTable = function(){
+  var table = document.getElementById('salesReport');
+  var row = document.createElement('tr');
+    var tableHeader = document.createElement('th');
+    tableHeader.textContent = this.location;
+    row.appendChild(tableHeader);
+  table.appendChild(row);
+    
+  for (var i = 0; i < this.hourlySales.length; i++){
+    var table = document.getElementById('salesReport');
+    var tableData = document.createElement('td');
+      tableData.textContent = this.hourlySales[i];
+    row.appendChild(tableData);
+  
   }
-}
-
-
-
-// Store.prototype.salesCell = function(){
-//   for (var i = 0; i < this.hourlySales.length; i++){
-//     var cellParent = document.getElementById('salesCell');
-//     var cellChild = document.createElement('td');
-//     cellChild.textContent = this.hourlySales[i];
-//     cellParent.appendChild(cellChild);
-//   }
-// }
+};
 
 // STORE STATS //
  var seaStore = new Store('Seattle', 23, 65, 6.3)
-//  var testStore = new Store ('Test', 21, 45, 5.2)
+ var tokStore = new Store('Tokyo', 29, 15, 2.6 )
+ var testStore = new Store('TEST', 11, 22, 3.3)
 
 // CONSOLE LOGS //
 seaStore.simulateSales();
 seaStore.getTotals();
-seaStore.salesHeader();
-seaStore.salesCell();
-console.log(seaStore.hourlySales);
+seaStore.renderTable();
+tokStore.simulateSales();
+tokStore.getTotals();
+tokStore.renderTable();
+
